a05eae883704ee19ba2614984b46b87572e4889d
신승엽 <syshin@sinseung-yeob-ui-MacBookAir.local>
2021-06-16 21:19:37 +0900

    first commit

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue.js!',
-    list: ['사과', '바나나', '딸기']
+    // list: ['사과', '바나나', '딸기'],
+    list: [
+      {id: 1, name: '슬라임', hp: 100},
+      {id: 2, name: '고블린', hp: 200},
+      {id: 3, name: '드래곤', hp: 500}
+    ],
+    ok: false
   },
   methods: {
     handleClick: function(e) {
